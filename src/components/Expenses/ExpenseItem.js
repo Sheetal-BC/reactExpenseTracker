ba24196592398807d3c2ecd4691eb22ab2eef759
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-07-20 12:20:39 +0530

    delete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,25 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem =(props) => {
+
+
+  const clickHandler = () =>{
+    
+    const element = document.querySelector('.expense-item');
+    if(element){
+    element.parentNode.removeChild(element);
+    }
+
+}
+  
   return (
-    <Card className='expense-item'>
+    <Card id='title' className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Delete Expenses</button>
     </Card>
   );
 }
