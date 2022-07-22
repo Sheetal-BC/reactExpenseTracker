1988146f320dc41662df7e8724b9dcc60934acab
Sheetal BC <93135955+Sheetal-BC@users.noreply.github.com>
2022-07-22 11:59:55 +0530

    single expenses to add on more

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -7,6 +7,19 @@ const ExpensesList = (props) => {
   if (props.items.length === 0) {
     return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
   }
+  if(props.items.length === 1){
+    return (
+      <div> {props.items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))}
+    <h2 className='expenses-list__fallback'>Only single expese here. Please add more...</h2>;
+    </div>
+  )}
 
   return (
     <ul className='expenses-list'>
